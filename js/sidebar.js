b40bb1481c80ea9d8ff3274b5f6310ad44f994e2
mayazarre <maya.isabellevi@gmail.com>
2017-07-22 10:56:00 -0700

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,6 @@
-function s_open() {
+function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
 }
-function s_close() {
+function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
 }
